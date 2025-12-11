da9416ed4012c79da895ed887cf84eabfe1d9ac6
amruta13gadekar <ammugadekar8@gmail.com>
2025-12-11 17:03:20 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,7 +1,7 @@
 def product_details(name,pro_id,quantity, price):
     result = (
-        f"product Name:{name}\n"
-        f"product ID:{pro_id}\n"
+        f"Product Name:{name}\n"
+        f"Product ID:{pro_id}\n"
         f"Quantity:{quantity}\n"
         f"Price:{price}"
     )
@@ -10,7 +10,7 @@ def product_details(name,pro_id,quantity, price):
 if __name__ == "__main__":
     name = "kitkat"
     pro_id = "p1001"
-    quantity = "20g"
+    quantity = "30g"
     salary = 20
     print(product_details(name,pro_id,quantity, price))
 
