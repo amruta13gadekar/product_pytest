4b0532a58068b3e9b9ef8258a723cc864e61dc0a
amruta13gadekar <ammugadekar8@gmail.com>
2025-12-11 17:03:59 +0530

    Update product quantity in test case

--- test_product.py
+++ test_product.py
@@ -4,9 +4,9 @@ def test_product_details():
     expected_output = (
         "Product Name:kitkat\n"
         "Product ID:p1001\n"
-        "Quantity:20g\n"
+        "Quantity:30g\n"
         "Price:20"
     )
-    assert product_details("kitkat","p1001","20g", 20) == expected_output
+    assert product_details("kitkat","p1001","20g", 30) == expected_output
 
 
